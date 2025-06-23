64e76a127f0f6fa2d22466505c0de3c41e435115
Yorkshireman <>
2025-06-23 21:27:54 +0100

    add code for system volume access (broken)

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+import SystemSetting from 'react-native-system-setting';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Index() {
   const [count, setCount] = useState(0);
+  const lastValue = useRef(0.5);
+
+  useEffect(() => {
+    const listener = SystemSetting.addVolumeListener(({ value }) => {
+      setCount(c => c + (value > lastValue.current ? 1 : value < lastValue.current ? -1 : 0));
+      lastValue.current = value;
+      SystemSetting.setVolume(0.5);
+    });
+
+    SystemSetting.setVolume(0.5);
+    return () => SystemSetting.removeVolumeListener(listener);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.count}>{count}</Text>
